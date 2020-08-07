531ab02c111bb23be3459762a2f06397b9ec5203
paderborner2019 <paderborner2019@gmail.com>
2020-08-07 18:25:39 +0300

    connect dapper, create database

---
+++ Backend/Data/DataRepository.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using Dapper;
+using QandA.Data.Models;
+
+namespace QandA.Data
+{
+    public class DataRepository : IDataRepository
+    {
+        private readonly string _connectionString;
+
+        public DataRepository(IConfiguration configuration)
+        {
+            _connectionString = configuration["ConnectionStrings:DefaultConnection"];
+        }
+
+        public IEnumerable<QuestionGetManyResponse> GetQuestions()
+        {
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                connection.Open();
+                return connection.Query<QuestionGetManyResponse>(@"EXEC dbo.Question_GetMany");
+            }
+        }
+        public AnswerGetResponse GetAnswer(int answerId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public QuestionGetSingleResponse GetQuestion(int questionId)
+        {
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                connection.Open();
+                var question =
+                connection.QueryFirstOrDefault<QuestionGetSingleResponse>(
+                @"EXEC dbo.Question_GetSingle @QuestionId = @QuestionId",
+                new { QuestionId = questionId }
+                );
+                return question;
+            }
+        }
+
+        public IEnumerable<QuestionGetManyResponse> GetQuestionsBySearch(string search)
+        {
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                connection.Open();
+                return connection.Query<QuestionGetManyResponse>(
+                    @"EXEC dbo.Question_GetMany_BySearch @Search = @Search",
+                    new { Search = search });
+            }
+        }
+
+        public IEnumerable<QuestionGetManyResponse> GetUnansweredQuestions()
+        {
+            using (var connection = new SqlConnection(_connectionString))
+                return connection.Query<QuestionGetManyResponse>(@"dbo.Question_GetUnanswered");
+        }
+
+        public bool QuestionExists(int questionId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Backend/Data/IDataRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using QandA.Data.Models;
+
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QandA.Data
+{
+    interface IDataRepository
+    {
+        IEnumerable<QuestionGetManyResponse> GetQuestions();
+        IEnumerable<QuestionGetManyResponse>
+        GetQuestionsBySearch(string search);
+        IEnumerable<QuestionGetManyResponse>
+        GetUnansweredQuestions();
+        QuestionGetSingleResponse
+        GetQuestion(int questionId);
+        bool QuestionExists(int questionId);
+        AnswerGetResponse GetAnswer(int answerId);
+    }
+}
